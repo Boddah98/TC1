6abc10a43a0414f546ba62841e3a71db4a6c8de7
Boddah98 <andy.acros@estudiantec.cr>
2023-08-12 16:39:11 -0600

    game component
    Ready for navigate to game component with a button on home component

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 //Creation of the server
-const express = require();
+const express = require('express');
 const app = express();
 
 //Port define
-const port = procress.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- preguntados/src/app/home/home.component.ts
+++ preguntados/src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['../../styles.css']
 })
-export class AppHome {
-  title = 'Preguntados';
+export class AppHome implements OnInit {
+  constructor (private router: Router){}
+  ngOnInit(): void {
+    this.init()
+  }
+  init(){
+    document.getElementById("playPreguntados")?.addEventListener('click', evt =>{
+      this.router.navigate(['/game']);
+    });
+  }
 }
