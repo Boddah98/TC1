69a792c7027c1cd9f28baa1f863877eb05c9e73d
Boddah98 <andy.acros@estudiantec.cr>
2023-08-12 03:07:13 -0600

    corrections

--- preguntados/src/app/app-routing.module.ts
+++ preguntados/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AppHome} from './home/home.component';
+import {AppGame} from './game/game.component';
 
 const routes: Routes = [
-  {path:'', component: AppHome}
+  {path:'', component: AppHome},
+  {path:'game', component: AppGame}
 ];
 
 @NgModule({
